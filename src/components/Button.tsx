9c0175919669a209aa0273813bf22f840e0ccfdc
hasnat5 <hasnatferdiananda5@gmail.com>
2023-09-03 13:20:26 +0700

    fix button font

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ const Button = () => {
         <button
             style={{ backgroundImage: 'linear-gradient(313deg, #6843EC 17.53%, #00FF68  94.64%)' }}
             // style={{ backgroundImage: 'linear-gradient(313deg, #00FF68 -0%, #6843EC 100%)' }}
-            className='rounded-lg py-2 px-4 text-white font-mono text-base'>
+            className='rounded-lg py-2 px-4 text-white font-mono text-base uppercase'>
             Get Started
         </button>
     )
