ffcfa1dee28c89ad105c753105ea0c534308ef5f
hasnat5 <hasnatferdiananda5@gmail.com>
2023-09-09 17:53:05 +0700

    fix benefit view

--- src/components/Benefit.tsx
+++ src/components/Benefit.tsx
@@ -5,13 +5,13 @@ import React from 'react'
 
 const Benefit: React.FC<Benefit> = (props) => {
     return (
-        <div className='grid gap-8 w-full bg-benefit-box bg-cover bg-no-repeat bg-top p-4 md:content-start lg:bg-contain'>
+        <div className='grid gap-8 w-full bg-benefit-box bg-cover bg-no-repeat bg-top p-4 md:content-start'>
             <div style={{ backgroundImage: 'linear-gradient(313deg, #6843EC 17.53%, #00FF68  94.64%)' }} className='rounded-xl h-12 w-12 grid justify-center content-center'>
                 <Icon icon={props.icon} color="white" width="24" />
             </div>
             <div className='grid gap-4'>
-                <h3 className='uppercase font-mono'>{props.title}</h3>
-                <p>{props.description}</p>
+                <h4 className='uppercase text-white'>{props.title}</h4>
+                <p className='text-justify'>{props.description}</p>
             </div>
         </div>
     )
