3069c070289b8b71bc06e4387e40f934178e7bde
hasnat5 <hasnatferdiananda5@gmail.com>
2023-09-11 14:17:17 +0700

    change image cta

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -7,14 +7,14 @@ const CTA = () => {
         <div style={{ backgroundImage: 'linear-gradient(313deg, #6843EC 17.53%, #00FF68  94.64%)' }} className='relative grid rounded-lg overflow-hidden lg:grid-cols-2'>
             <div className='grid gap-4 items-center content-center px-6 py-12 md:gap-6 md:py-20 md:px-16'>
                 <h3>Mari mulai membangun perjalanan finansial bebas utang untuk masa depan Anda.</h3>
-                <ButtonSec />
+                <ButtonSec title='get started' link='/appropriateness' />
             </div>
 
-            <div className='grid w-full relative lg:items-end lg:w-[150%]'>
+            <div className='grid w-full relative lg:items-end lg:w-[130%]'>
                 <Image
-                    src="https://assets.website-files.com/63b8916a2666a32bc26a89ba/63b8916a2666a315fe6a89fe_image-1-hero-v1-dataplus-template-p-1600.png"
+                    src="/assets/images/fitur-kelayakan.webp"
                     alt="CTA"
-                    className="w-full h-auto rounded-2xl ml-6 -mb-24 md:-mb-52 md:ml-16 lg:-mb-56 lg:ml-0"
+                    className="w-full h-auto rounded-2xl ml-6 -mb-24 md:-mb-52 md:ml-16 lg:-mb-40 lg:ml-0"
                     width={1468}
                     height={1268}
                 />
